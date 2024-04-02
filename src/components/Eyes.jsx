b2c119a0f4e2d9fe720647c275e995d4e934c366
Amangoua Ange <adagou@yahoo.com>
2024-04-02 12:44:06 +0000

    updating eyes

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from "react";
+
 export default function Eyes() {
+  const [rotate, setRotate] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", function (e) {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      const deltaX = mouseX - this.window.innerWidth / 2;
+      const deltaY = mouseY - this.window.innerHeight / 2;
+
+      let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotate(angle - 180);
+    });
+  }, []);
+
   return (
     <div className="w-full -z-10 h-screen overflow-hidden">
       <div className="relative bg-eyes h-full w-full bg-cover bg-center bg-no-repeat">
         <div className="absolute gap-10 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 flex justify-between">
           <div className="w-[16rem] h-[16rem] relative bg-white rounded-full flex justify-center items-center">
-            <div className="w-2/3 h-2/3 flex items-center bg-black rounded-full ">
+            <div className="w-2/3 h-2/3 flex items-center relative bg-black rounded-full">
               <span className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-2xl">
                 play
               </span>
-              <span className="h-10 w-full relative rotate-90 flex items-center px-1">
-                <span className="h-6 w-6 bg-white absolute rounded-full"></span>
+              <span
+                style={{
+                  transform: `rotate(${rotate}deg)`,
+                }}
+                className="h-10 w-full relative flex items-center px-1"
+              >
+                <span className="h-6 w-6 bg-white rounded-full"></span>
               </span>
             </div>
           </div>
@@ -18,8 +40,13 @@ export default function Eyes() {
               <span className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-2xl">
                 play
               </span>
-              <span className="h-10 w-full relative rotate-0 flex items-center px-1">
-                <span className="h-6 w-6 bg-white absolute rounded-full"></span>
+              <span
+                style={{
+                  transform: `rotate(${rotate}deg)`,
+                }}
+                className="h-10 w-full relative flex items-center px-1"
+              >
+                <span className="h-6 w-6 bg-white rounded-full"></span>
               </span>
             </div>
           </div>
