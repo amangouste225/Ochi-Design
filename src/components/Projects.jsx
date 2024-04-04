071718cc323ac93dca8d5599bf39e1f8885b24db
Amangoua Ange <adagou@yahoo.com>
2024-04-04 20:06:54 +0000

    change title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <div className="w-full bg-[#F1F1F1] py-32">
       <h1 className="text-black px-10 font-montreal whitespace-wrap text-[3.4vw] w-4/5 leading-tight font-normal">
-        Featured projects
+        Featured projectss
       </h1>
     </div>
   );
